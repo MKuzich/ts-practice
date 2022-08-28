fa20090741045b45f4da31259244d66f440c1dbb
Mykhailo Kuzich <timmkedth@gmail.com>
2022-08-28 21:01:37 +0300

    2 task

--- app.ts
+++ app.ts
@@ -9,3 +9,77 @@ function add(num1: number, num2: number) {
 button.addEventListener("click", function () {
   console.log(add(+input1.value, +input2.value));
 });
+S;
+let age: number = 50;
+let name: string = "Max";
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: undefined = undefined;
+let callback: Function = (a: number) => {
+  return 100 + a;
+};
+
+let anything: any = -20;
+anything = "Text";
+anything = {};
+
+let some: unknown;
+some = "Text";
+
+let str: string;
+
+if (typeof some === "string") {
+  str = some;
+}
+
+let person: [string, number];
+
+enum Preparing {
+  LOADING,
+  READY,
+}
+
+let strOrNum: string | number;
+
+let toggler: "enable" | "disable";
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error("Error");
+}
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: string;
+    updateAt: string;
+  };
+};
+
+const page1: Page = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
+
+const page2: Page = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
